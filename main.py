1f3c1b880dd14aa78982beebc390877d020c7e92
andre <xerand73@gmail.com>
2026-03-11 20:22:18 +0100

    before collisions

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -13,7 +15,16 @@ def main():
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+        
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
+    
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidField = AsteroidField()
     
     while True:
         log_state()
@@ -23,16 +34,15 @@ def main():
                 return
         
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
         
+        for draw_obj in drawable:
+            draw_obj.draw(screen)
+
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
         
-        
-        
-    
-
 
 if __name__ == "__main__":
     main()
